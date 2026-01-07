a46486eb1e8bc0ad89924b0c31a4e052ccb63106
Somu <100427492+SomuRanjanChanda@users.noreply.github.com>
2026-01-07 22:34:23 +0530

    Add files via upload

---
+++ 6th.py
@@ -0,0 +1,8 @@
+Movies=[]
+movie1=input("Enter the name of the movie 1: ")
+movie2=input("Enter the name of the movie 2: ")
+movie3=input("Enter the name of the movie 3: ")
+Movies.append(movie1)
+Movies.append(movie2)
+Movies.append(movie3)
+print(Movies)
---
+++ 7th.py
@@ -0,0 +1,7 @@
+
+#storing multiple key-value pairs in a single dictionary
+dictionary = {
+"table": ("a piece of furniture","list of facts and figures"),
+"cat" : "a small animal"
+}
+print(dictionary)
