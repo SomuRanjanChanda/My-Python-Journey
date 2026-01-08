75446f0ddf7d26e83e4da878c57712ac0aedab1c
Somu <100427492+SomuRanjanChanda@users.noreply.github.com>
2026-01-08 22:39:31 +0530

    Add files via upload

---
+++ 8th.py
@@ -0,0 +1,5 @@
+set = {
+    "python", "java", "c++", "python", "javascript","java","python", "c++","c"
+    }           #values
+
+print(len(set)) #length of set
---
+++ 9th.py
@@ -0,0 +1,5 @@
+dictionary = {}
+dictionary.update({"math" : 90})
+dictionary.update({"science" : 85})
+dictionary.update({"english" : 88})
+print(dictionary)
